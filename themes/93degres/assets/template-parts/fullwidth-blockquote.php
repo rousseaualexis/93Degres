5868e3efa355a0f833717dbd293fe001dff2afeb
Alexis Rousseau <ialexisrousseaui@gmail.com>
2018-04-09 14:00:47 +0200

    update .post:hover

---
+++ themes/93degres/assets/template-parts/fullwidth-blockquote.php
@@ -0,0 +1,8 @@
+<?php
+	$blockquote = get_sub_field('blockquote');
+?>
+<div class="full-width full-width__blockquote col-xs-40 col-md-32 col-md-offset-8">
+    <blockquote>
+   	    <h3> <?php echo $blockquote; ?> </h3>
+    </blockquote>
+</div>
