cc0cf10d14eda1fb733affbe5f0fa7e5f3c51e65
Alexis Rousseau <ialexisrousseaui@gmail.com>
2018-05-09 20:40:48 +0200

    add marquee to animation

--- themes/93degres/assets/js/main.js
+++ themes/93degres/assets/js/main.js
@@ -274,12 +274,13 @@ var homepage = (function() {
             splitCategories = new SplitText($categories,{charsClass: "charsplit", wordsClass: "wordsplit"});
             $("h1").prepend('<div class="trait"></div>');
         var tl = new TimelineLite();
-            tl.from($el.find('.image'), 1.8, {y:'200%', ease:Power4.easeOut}, 0.2);
+            tl.from($el.find('.country-code'), 1.8, { alpha:'0', ease:Power4.easeOut}, '+=0.6');
+            tl.from($el.find('.image'), 1.8, {y:'200%', ease:Power4.easeOut}, '-=1.6');
             tl.from($el.find('.categories img'), 0.6, {y:'300%', ease:Power2.easeOut}, '-=1.2');
             tl.staggerFrom($el.find('.categories .wordsplit'), 0.6, {y:'300%', ease:Power2.easeOut}, 0.1, '-=1.1');
             tl.staggerFrom($el.find('h1 .charsplit'), 1.2, {y:'150%', ease:Power4.easeOut}, 0.01, '-=1');
-            tl.from($el.find('.trait'), 0.6, {scaleX:'0', transformOrigin:"left", ease:Power4.easeOut}, 2);
             tl.from($el.find('strong'), 0.8, {left:'-120%', ease:Power4.easeOut}, '-=0.8');
+            tl.from($el.find('.trait'), 0.6, {scaleX:'0', transformOrigin:"left", ease:Power4.easeOut}, '-=0.4');
             tl.from($el.find('.a-cta'), 1, {opacity:0, y:'300%', ease:Power4.easeOut}, '-=1.6');
     }
 
