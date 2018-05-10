f586a497c6cda1090281372bb3a87dc0319d0f3f
Alexis Rousseau <ialexisrousseaui@gmail.com>
2018-05-10 23:16:03 +0200

    modify cover homepage

--- themes/93degres/assets/js/main.js
+++ themes/93degres/assets/js/main.js
@@ -286,9 +286,9 @@ var homepage = (function() {
             tl.staggerFrom($el.find('.categories .wordsplit'), 0.6, {y:'300%', ease:Power2.easeOut}, 0.1, '-=1.1');
             tl.staggerFrom($el.find('h1 .charsplit'), 1.2, {y:'150%', ease:Power4.easeOut}, 0.01, '-=1');
             tl.from($el.find('strong'), 0.8, {left:'-120%', ease:Power4.easeOut}, '-=0.8');
-            tl.from($el.find('.a-cta'), 1, {opacity:0, y:'300%', ease:Power4.easeOut}, '-=0.8');
-            tl.from($el.find('.trait'), 0.6, {scaleX:'0', transformOrigin:"left", ease:Power4.easeOut}, '-=0.4');
-            tl.from($el.find('.label'), 0.4, {scale:'1.6', alpha:'0', ease:Power2.easeOut}, '-=0.6');
+            tl.from($el.find('.a-cta'), 1, {opacity:0, y:'300', ease:Power4.easeOut}, '-=1');
+            tl.from($el.find('.trait'), 0.6, {scaleX:'0', transformOrigin:"left", ease:Power4.easeOut}, '-=1');
+            tl.from($el.find('.label'), 0.4, {display:'none', scale:'1.4', ease:Power2.easeOut}, '-=0.6');
     }
 
     return {
