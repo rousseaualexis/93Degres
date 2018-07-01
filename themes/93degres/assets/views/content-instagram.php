6c121c8aba3ccd358998ab9ef88b9bcfca14710b
Alexis Rousseau <ialexisrousseaui@gmail.com>
2018-07-01 15:14:12 +0200

    modify instagram

--- themes/93degres/assets/views/content-instagram.php
+++ themes/93degres/assets/views/content-instagram.php
@@ -1,6 +1,7 @@
 <?php
 $page_id = '5748305008';
-$access_token = '5748305008.1677ed0.5b2118fbb69246c8b6a8e05ebe512287';
+
+$access_token = '5748305008.1677ed0.8a7a9cfb55b04e339e089cf75ce45f36';
 $json_object = @file_get_contents('https://api.instagram.com/v1/users/' . $page_id . 
 '/media/recent/?access_token=' . $access_token . '&count=4');
 $indata = json_decode($json_object);
@@ -22,22 +23,22 @@ $indata = json_decode($json_object);
     <ul class="scroll-reveal instagram_shots">
         <li class="anime">
             <a href="<?php echo $indata->data[0]->link; ?>" class="instalink" target="_blank">
-                <img src="<?php echo $indata->data[0]->images->low_resolution->url; ?>" />
+                <img src="<?php echo $indata->data[0]->images->standard_resolution->url; ?>" />
             </a>
         </li>
         <li class="anime">
             <a href="<?php echo $indata->data[1]->link; ?>" class="instalink" target="_blank">
-                <img src="<?php echo $indata->data[1]->images->low_resolution->url; ?>" />
+                <img src="<?php echo $indata->data[1]->images->standard_resolution->url; ?>" />
             </a>
         </li>
         <li class="anime">
             <a href="<?php echo $indata->data[2]->link; ?>" class="instalink" target="_blank">
-                <img src="<?php echo $indata->data[2]->images->low_resolution->url; ?>" />
+                <img src="<?php echo $indata->data[2]->images->standard_resolution->url; ?>" />
             </a>
         </li>
         <li class="anime">
             <a href="<?php echo $indata->data[3]->link; ?>" class="instalink" target="_blank">
-                <img src="<?php echo $indata->data[3]->images->low_resolution->url; ?>" />
+                <img src="<?php echo $indata->data[3]->images->standard_resolution->url; ?>" />
             </a>
         </li>
     </ul>
